ed7471f91e77d3f38b2e8e8586611fd7ae0cde19
Unknown <daniel.zimmerman@gmail.com>
2018-10-24 16:18:08 -0600

    Added media query for h1 title

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,60 +1,101 @@
-import React, { Component, Fragment } from 'react'
-import './App.css'
-import Header from './Header'
-import Footer from './Footer'
-
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 class HomePage extends Component {
   render() {
     return (
       <Fragment>
-          <Header />
+        <Header />
 
-
-          <header>
-
-            <div className="self-portrait">
-              <img
-                src="https://lh3.googleusercontent.com/Zpbkpy39-lbYn6LKEP6O7Jnof1fVWNWfi7E43Q44Bm9iLGXLXltbfaRYcZE6l_wpruiTQiURkaF_ZiQuhm47HCcAdgiGo4NjXU5KA9Ki6N_AtXZOACcRZkqDqATIXf_zYLMSCvzNfndX_0hV7cmByMoi_FLShktHuwztW8JU56HU4MphvbiO9eXf76cBrgag2AjZF2pSx8-aetGsdH0EEi1iBYhmKtV1YmtSL3sA1UiaVTE0lUVDyYs1yPEa-lJyu1V-Z_80PK9AuutXTx6VMrr9xPEBR2TDyDIYjynUvT-Gl_PmkE9rzHYVXxCJHNrQRVhiclL2K6Z4mQGX1lD06WsZteN9vMpBWnikAh9SACPZF-DFaT-RwRJ-D6g4payA_qKFb1NKyUvOzxYqNWgAoPJx2ZTKd7gNgo8rURCssu6lV3MdX5mTNiFo14e7cft67LpE3JKymT0S4XMM7n8pW3rmWwgkQjpfln1MJda8H933IHCPhs1m8OxXtbLt_LPdPwrKplGwLHlC4TYYZbk0nBd73qnyVLyZaTsURIonpNa-HEp4JFNJK4L1JOd6fFrfaackQWUe8whCdzR8wNAYN1nSMiA2HdAdCgDEjEkqjljeGnwTCVIcbcqIFFUwxkqRLIxiIemTWhJwZqTVL3GGnC4N0Tx5EsmM=w408-h610-no"
-                alt="Daniel Zimmerman" className="imagehomepage" />
-            </div>
-          </header>
-
-          <div>
-            <h2>Current Skills</h2>
+        <header>
+          <div className="self-portrait">
+            <img
+              src="https://lh3.googleusercontent.com/Zpbkpy39-lbYn6LKEP6O7Jnof1fVWNWfi7E43Q44Bm9iLGXLXltbfaRYcZE6l_wpruiTQiURkaF_ZiQuhm47HCcAdgiGo4NjXU5KA9Ki6N_AtXZOACcRZkqDqATIXf_zYLMSCvzNfndX_0hV7cmByMoi_FLShktHuwztW8JU56HU4MphvbiO9eXf76cBrgag2AjZF2pSx8-aetGsdH0EEi1iBYhmKtV1YmtSL3sA1UiaVTE0lUVDyYs1yPEa-lJyu1V-Z_80PK9AuutXTx6VMrr9xPEBR2TDyDIYjynUvT-Gl_PmkE9rzHYVXxCJHNrQRVhiclL2K6Z4mQGX1lD06WsZteN9vMpBWnikAh9SACPZF-DFaT-RwRJ-D6g4payA_qKFb1NKyUvOzxYqNWgAoPJx2ZTKd7gNgo8rURCssu6lV3MdX5mTNiFo14e7cft67LpE3JKymT0S4XMM7n8pW3rmWwgkQjpfln1MJda8H933IHCPhs1m8OxXtbLt_LPdPwrKplGwLHlC4TYYZbk0nBd73qnyVLyZaTsURIonpNa-HEp4JFNJK4L1JOd6fFrfaackQWUe8whCdzR8wNAYN1nSMiA2HdAdCgDEjEkqjljeGnwTCVIcbcqIFFUwxkqRLIxiIemTWhJwZqTVL3GGnC4N0Tx5EsmM=w408-h610-no"
+              alt="Daniel Zimmerman"
+              className="imagehomepage"
+            />
           </div>
-          <div className="skills-section">
-            <a href="https://reactjs.org/" target="blank"><img
+        </header>
+
+        <div>
+          <h2>Current Skills</h2>
+        </div>
+        <div className="skills-section">
+          <a href="https://reactjs.org/" target="blank">
+            <img
               src="https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png"
-              alt="React Logo" className="logoimages" /></a>
-            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="blank"><img
+              alt="React Logo"
+              className="logoimages"
+            />
+          </a>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="blank"
+          >
+            <img
               src="https://appendto.com/wp-content/uploads/2016/05/javascript-e1477353149155.png"
-              alt="JS Logo" className="logoimages" /></a>
-            <a href="https://nodejs.org/en/" target="blank"><img
+              alt="JS Logo"
+              className="logoimages"
+            />
+          </a>
+          <a href="https://nodejs.org/en/" target="blank">
+            <img
               src="https://cdn-images-1.medium.com/max/2000/1*q9ww_u32hhpMaA-Q_s1ujw.png"
-              alt="NodeJS Logo" className="logoimages" /></a>
-            <a href="https://css-tricks.com/" target="blank"><img
+              alt="NodeJS Logo"
+              className="logoimages"
+            />
+          </a>
+          <a href="https://css-tricks.com/" target="blank">
+            <img
               src="https://www.lifewire.com/thmb/zrcb1zA4Vi47uULJhJKJdcshBgk=/1024x1024/filters:fill(auto,1)/css3-57b597e85f9b58b5c2b338de.png"
-              alt="CSS Logo" className="logoimages" /></a>
-            <a href="https://firebase.google.com/" target="blank"><img
+              alt="CSS Logo"
+              className="logoimages"
+            />
+          </a>
+          <a href="https://firebase.google.com/" target="blank">
+            <img
               src="https://ih0.redbubble.net/image.489553250.2202/flat,550x550,075,f.jpg"
-              alt="Firebase Logo" className="logoimages" /></a>
-            <a href="https://www.gitkraken.com/" target="blank"><img
+              alt="Firebase Logo"
+              className="logoimages"
+            />
+          </a>
+          <a href="https://www.gitkraken.com/" target="blank">
+            <img
               src="https://rlv.zcache.co.uk/gitkraken_stickers-r514220fb582c40a88e313a74d7660fe6_v9i40_8byvr_540.jpg"
-              alt="Logo" className="logoimages" /></a>
-            <a href="https://github.com/" target="blank"><img
+              alt="Logo"
+              className="logoimages"
+            />
+          </a>
+          <a href="https://github.com/" target="blank">
+            <img
               src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png"
-              alt="Git Hub Logo" className="logoimages" /></a>
-            <a href="https://www.mongodb.com/" target="blank"><img
+              alt="Git Hub Logo"
+              className="logoimages"
+            />
+          </a>
+          <a href="https://www.mongodb.com/" target="blank">
+            <img
               src="https://cacm.acm.org/system/assets/0002/7119/042117_Theodo_MongoDB.large.jpg?1492791427&1492791427"
-              alt="MongoDB Logo" className="logoimages" /></a>
-          </div>
+              alt="MongoDB Logo"
+              className="logoimages"
+            />
+          </a>
+        </div>
+        <div className="codewars-section">
+          <h4>Codewars</h4>
+          <img
+            src="https://www.codewars.com/users/dtzimmer/badges/large"
+            alt="Codewars"
+            className="codewars-icon"
+          />
+        </div>
 
-          <Footer />
+        <Footer />
       </Fragment>
-    )
+    );
   }
-
 }
 
-export default HomePage
+export default HomePage;
--- src/WorkExperience.js
+++ src/WorkExperience.js
@@ -1,131 +1,164 @@
-import React, { Component, Fragment } from 'react'
-import './App.css'
-import Helio from './HelioGrad.jpg'
-import Hawthorn from './Hawthorn.jpg'
-import Header from './Header'
-import Footer from './Footer'
-
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import Helio from "./HelioGrad.jpg";
+import Hawthorn from "./Hawthorn.jpg";
+import Header from "./Header";
+import Footer from "./Footer";
 
 class WorkExperience extends Component {
   render() {
     return (
       <Fragment>
-          <Header />
-            <h2>Education and Work Experience</h2>
-          <section className="resume-section">
-            <div className="container">
-              <div className="row">
-                <div className="col">
-                  <div>
-                    <h3> Education </h3>
-                    <img
-                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Brigham_Young_University_medallion.svg/2000px-Brigham_Young_University_medallion.svg.png"
-                      alt="byu" className="resumeimages" />
-                    <p>BS Elementary Education (K-6)</p>
-                  </div>
+        <Header />
+        <h2>Work Experience</h2>
+        <section className="resume-section">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <div>
+                  <h4> Education </h4>
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Brigham_Young_University_medallion.svg/2000px-Brigham_Young_University_medallion.svg.png"
+                    alt="byu"
+                    className="resumeimages"
+                  />
+                  <h4>BS Elementary Education (K-6)</h4>
                 </div>
-                <div className="col">
-                  <div>
-                    <h3> Technology Training </h3>
-                    <img
-                      src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/3954/s300/hel-stackedlogo-onwhite.png"
-                      alt="Logo" className="resumeimages" />
-                    <p>Certification of Competency</p>
-                  </div>
+              </div>
+              <div className="col">
+                <div>
+                  <h4> Technology Training </h4>
+                  <img
+                    src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/3954/s300/hel-stackedlogo-onwhite.png"
+                    alt="Logo"
+                    className="resumeimages"
+                  />
+                  <h4>Certification of Competency</h4>
                 </div>
               </div>
             </div>
-          </section>
-          <section className="resume-section">
-            <div>
-              <a href="https://drive.google.com/file/d/1PumvBjIAxutSaBwB80Ch-ZpBE8wzjQ73/view?usp=sharing"
-                 target="blank">
-                <button className="resume-button">Download My Resume</button>
-              </a>
-            </div>
-          </section>
+          </div>
+        </section>
+        <section className="resume-section">
+          <div>
+            <a
+              href="https://drive.google.com/file/d/1PumvBjIAxutSaBwB80Ch-ZpBE8wzjQ73/view?usp=sharing"
+              target="blank"
+            >
+              <button className="resume-button">Download My Resume</button>
+            </a>
+          </div>
+        </section>
 
-          <header className="backgroundimagesections">
-            <h2>Work History</h2>
-          </header>
+        <header className="backgroundimagesections">
+          <h2>Work History</h2>
+        </header>
 
-      <section className="workhistory-section">
-        <div className="workhistory-card">
-          <div className="row">
-            <div className="col-sm-4">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Brigham_Young_University_medallion.svg/2000px-Brigham_Young_University_medallion.svg.png" alt="BYU" className="workhistory-img"/>
-            </div>
-            <div className="col-sm-8">
-              <div className="slide slide-top col">
-                <div className="slide-content">
-                  <h3>
-                    In 2014, I graduated with a B.S. in Elementary Education (K-6)
-                  </h3>
-                 </div>
-              <img src="https://www.byu.edu/oncampushousing/images/Helaman_birds_eye.jpg" alt="BYU" className="slide-img"/>
+        <section className="workhistory-section">
+          <div className="workhistory-card">
+            <div className="row">
+              <div className="col-sm-4">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Brigham_Young_University_medallion.svg/2000px-Brigham_Young_University_medallion.svg.png"
+                  alt="BYU"
+                  className="workhistory-img"
+                />
+              </div>
+              <div className="col-sm-8">
+                <div className="slide slide-top col">
+                  <div className="slide-content">
+                    <h3>
+                      In 2014, I graduated with a B.S. in Elementary Education
+                      (K-6)
+                    </h3>
+                  </div>
+                  <img
+                    src="https://www.byu.edu/oncampushousing/images/Helaman_birds_eye.jpg"
+                    alt="BYU"
+                    className="slide-img"
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        </div>
 
-        <div className="workhistory-card">
-          <div className="row">
-            <div className="col-sm-4">
-              <img src="http://static1.squarespace.com/static/5812896dd482e97aca1ff170/t/591e175abe6594debd1a8ca5/1534808806708/" alt="Texas State" className="workhistory-img"/>
-            </div>
-            <div className="col-sm-8">
-              <div className="slide slide-top col">
-                <div className="slide-content">
-                  <h3>
-                  Moved to Houston, TX and taught 5th grade math/science for 3 years
-                  </h3>
-                 </div>
-                 <img src="http://mrgaragedoor.com/wp-content/uploads/sites/2/2015/07/GarageDoorHoustonImage.jpg" alt="Houston TX" className="slide-img"/>
+          <div className="workhistory-card">
+            <div className="row">
+              <div className="col-sm-4">
+                <img
+                  src="http://static1.squarespace.com/static/5812896dd482e97aca1ff170/t/591e175abe6594debd1a8ca5/1534808806708/"
+                  alt="Texas State"
+                  className="workhistory-img"
+                />
+              </div>
+              <div className="col-sm-8">
+                <div className="slide slide-top col">
+                  <div className="slide-content">
+                    <h3>
+                      Moved to Houston, TX and taught 5th grade math/science for
+                      3 years
+                    </h3>
+                  </div>
+                  <img
+                    src="http://mrgaragedoor.com/wp-content/uploads/sites/2/2015/07/GarageDoorHoustonImage.jpg"
+                    alt="Houston TX"
+                    className="slide-img"
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        </div>
 
-        <div className="workhistory-card">
-          <div className="row">
-            <div className="col-sm-4">
-              <img src="https://hawthornacademy.org/assets/images/footer-logo.png" alt="hawthorn" className="workhistory-img"/>
-            </div>
-            <div className="col-sm-8">
-              <div className="slide slide-top col">
-                <div className="slide-content">
-                  <h3>
-                  Moved back to Sandy, UT and taught 4th grade at Hawthorn Academy for 1 year
-                  </h3>
-                 </div>
-                <img src={Hawthorn} alt="Hawthorn" className="slide-img"/>
+          <div className="workhistory-card">
+            <div className="row">
+              <div className="col-sm-4">
+                <img
+                  src="https://hawthornacademy.org/assets/images/footer-logo.png"
+                  alt="hawthorn"
+                  className="workhistory-img"
+                />
+              </div>
+              <div className="col-sm-8">
+                <div className="slide slide-top col">
+                  <div className="slide-content">
+                    <h3>
+                      Moved back to Sandy, UT and taught 4th grade at Hawthorn
+                      Academy for 1 year
+                    </h3>
+                  </div>
+                  <img src={Hawthorn} alt="Hawthorn" className="slide-img" />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        </div>
 
-        <div className="workhistory-card">
-          <div className="row">
-            <div className="col-sm-4">
-              <img src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/3954/s300/hel-stackedlogo-onwhite.png" alt="Helio Logo" className="workhistory-img"/>
-            </div>
-            <div className="col-sm-8">
-              <div className="slide slide-top col">
-                <div className="slide-content">
-                  <h3>
-                  Attended Helio Training and gained proficiency in full-stack web development                  
-                  </h3> 
-                 </div>
-                <img src={Helio} alt="Helio" className="slide-img"/>
+          <div className="workhistory-card">
+            <div className="row">
+              <div className="col-sm-4">
+                <img
+                  src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/3954/s300/hel-stackedlogo-onwhite.png"
+                  alt="Helio Logo"
+                  className="workhistory-img"
+                />
+              </div>
+              <div className="col-sm-8">
+                <div className="slide slide-top col">
+                  <div className="slide-content">
+                    <h3>
+                      Attended Helio Training and gained proficiency in
+                      full-stack web development
+                    </h3>
+                  </div>
+                  <img src={Helio} alt="Helio" className="slide-img" />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        </div>
-      </section>
-          <Footer />
+        </section>
+        <Footer />
       </Fragment>
-    )
+    );
   }
 }
 
-export default WorkExperience
+export default WorkExperience;
